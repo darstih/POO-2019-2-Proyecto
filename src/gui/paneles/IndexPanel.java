169d9f0822c8eb4d639308088f35d8991ff1bcdc
David Aristizabal <davidaristizabalgiraldo@gmail.com>
2020-03-22 22:22:43 -0500

    Panel de inicio

--- src/gui/paneles/IndexPanel.java
+++ src/gui/paneles/IndexPanel.java
@@ -1,9 +1,9 @@
 package gui.paneles;
 
-import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
@@ -15,17 +15,37 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
-public class IndexPanel extends Application{
+public class IndexPanel extends Pane{
 		
-	private static String[] pathImagenes = {"","","","",""};
+	private static String[] pathImagenes = {"../../Imagenes/imagen1.jpg","../../Imagenes/imagen2.jpg",
+			"../../Imagenes/imagen3.jpg","../../Imagenes/imagen4.jpg","../../Imagenes/imagen5.jpg"};
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		GridPane p0 = new GridPane();
+	private static String[] nombres = {"Darwin Stiven Herrera Cartagena",
+			"Alejandro Bedoya Taborda    ","David Antonio Aristizabal Giraldo"}; 
+	
+	private static String[] edades = {"20","20","19"};
+	
+	private static String[] correos = {"dsherrerac@unal.edu.co","albedoyat@unal.edu.co","daaristizabalg@unal.edu.co"};
+	
+	private static String[] fechas = {"15/08/1999","24/09/1999","24/01/2001"};
+	
+	private static String[] pathFotos = {"../../Imagenes/fotoDarwin.jpeg",
+			"../../Imagenes/fotoAlejandro.jpeg","../../Imagenes/fotoDavid.jpeg"};
+	
+	private static Label ltxtNombre,ltxtEdad,ltxtCorreo,ltxtFecha;
+	private Image imgFoto,imagen; 
+	private ImageView imgF,imga;
+	
+	private GridPane p0;
+	private static int cntI = 0;
+	private static int cntD = 0;
+	
+	public IndexPanel() throws Exception {
+		p0 = new GridPane();
 		
 		GridPane p1 = new GridPane();
 		GridPane p2 = new GridPane();
@@ -52,13 +72,6 @@ public class IndexPanel extends Application{
 		p4.setMaxSize(300, 400);
 		
 		
-		
-		
-	//	p1.setStyle("-fx-background-color: #2196f3;");
-	//	p2.setStyle("-fx-background-color: #ff9800;");
-	//	p3.setStyle("-fx-background-color: #9e9e9e;");
-//		p4.setStyle("-fx-background-color: #795548;");
-		
 		VBox P5 = new VBox(p1,p2);
 		
 		VBox P6 = new VBox(p3,p4);
@@ -66,6 +79,7 @@ public class IndexPanel extends Application{
 		p0.add(P5,0,0);
 		p0.add(P6,1,0);
 		
+		//ELEMENTOS VALIDACION
 		Label lblLogin = new Label("Iniciar seccion ");
 		Label lblUsu = new Label("Usuario: ");
 		Label lbli = new Label("¿No tienes cuenta?");
@@ -75,8 +89,33 @@ public class IndexPanel extends Application{
 		PasswordField pssfUsu = new PasswordField();
 		Button btnAceptar = new Button("Aceptar");
 		Button btnInvitado = new Button("Acceder como invitado");
-		Image imagen = new Image(getClass().getResourceAsStream("/../monalisa.jpg"));
-		ImageView imga = new ImageView(imagen);
+		lblLogin.setFont(new Font(15));
+		
+		//ELEMENTOS HOJA DE VIDA
+		imgFoto = new Image(getClass().getResourceAsStream(pathFotos[cntD%3]));
+		imgF = new ImageView(imgFoto);
+		Label lblCambiar = new Label("");
+		Label lblNombre = new Label("Nombre");
+		Label lblEdad = new Label("Edad");
+		Label lblCorreo = new Label("Correo");
+		Label lblFecha = new Label("Fecha de Nacimiento");
+		ltxtNombre = new Label(nombres[cntD%3]);
+		ltxtEdad = new Label(edades[cntD%3]);
+		ltxtCorreo = new Label(correos[cntD%3]);
+		ltxtFecha = new Label(fechas[cntD%3]);
+		imgF.setFitWidth(100);
+		imgF.setFitHeight(120);
+		lblCambiar.setMaxHeight(Double.MAX_VALUE);
+		lblCambiar.setMaxWidth(Double.MAX_VALUE);
+		imgF.setStyle("-fx-background-color: #795548;");
+		//lblCambiar.setStyle("-fx-background-color: #795548;");
+		
+		//ELEMENTOS IMAGENES ASOCIADAS
+		imagen = new Image(getClass().getResourceAsStream(pathImagenes[cntI%5]));
+		imga = new ImageView(imagen);
+		imga.setFitWidth(250);
+		imga.setFitHeight(200);
+		
 
 		btnAceptar.setMaxWidth(Double.MAX_VALUE);
 		btnInvitado.setMaxWidth(Double.MAX_VALUE);
@@ -86,9 +125,28 @@ public class IndexPanel extends Application{
 		lblLogin.setAlignment(Pos.CENTER);
 		lblsaludo.setFont(new Font(18));
 		
+		
+		
 		p1.add(imga, 0, 0);
+		
+		//PANEL HOJA DE VIDA
+		p2.add(imgF, 1, 0,2,1);
+		p2.add(lblNombre, 0, 1,2,1);
+		p2.add(ltxtNombre, 0, 2,2,1);
+		p2.add(lblEdad, 0, 3,2,1);
+		p2.add(ltxtEdad, 0, 4,2,1);
+		p2.add(lblCorreo, 0, 5,2,1);
+		p2.add(ltxtCorreo, 0, 6,2,1);
+		p2.add(lblFecha, 0, 7,2,1);
+		p2.add(ltxtFecha, 0, 8,2,1);
+		p2.setVgap(15);
+		p2.add(lblCambiar,0,0,7,10);
+		p2.setPadding(new Insets(10,10,10,10));
+		
 		p3.setRight(barraMenu);
 		p3.setCenter(lblsaludo);
+		
+		//PANEL LOGIN
 		p4.add(lblLogin,0,0,2,1);	
 		p4.add(lblUsu,0,1);	
 		p4.add(lblPass,0,2);
@@ -97,15 +155,40 @@ public class IndexPanel extends Application{
 		p4.add(btnAceptar,0,3,2,1);
 		p4.add(lbli,0,4,2,1);	
 		p4.add(btnInvitado,0,5,2,1);
-		p4.setVgap(30);
+
 		p4.setPadding(new Insets(10,10,10,10));
+		p4.setVgap(30);
 		
-		Scene a = new Scene(p0,600,600);
-		primaryStage.setScene(a);
-		primaryStage.show();
+		//ESCUCHADORES
+		LblHandlerClassH lblHandlerClass = new LblHandlerClassH();
+		lblCambiar.setOnMouseClicked(lblHandlerClass);
+		ImgHandlerClassImg handlerFotos = new ImgHandlerClassImg();
+		imga.setOnMouseEntered(handlerFotos);
 	}
-    public static void main(String[] args) {
-    	launch(args);
-    	
+	
+	private class LblHandlerClassH implements EventHandler<Event>{
+    	@Override
+		public void handle(Event event) {
+			cntD++;
+			imgFoto = new Image(getClass().getResourceAsStream(pathFotos[cntD%3]));
+			imgF.setImage(imgFoto);
+			ltxtNombre.setText(nombres[cntD%3]);
+			ltxtEdad.setText(edades[cntD%3]);
+			ltxtFecha.setText(fechas[cntD%3]);
+			ltxtCorreo.setText(correos[cntD%3]);
+		}
     }
+    
+    private class ImgHandlerClassImg implements EventHandler<Event>{
+    	@Override
+		public void handle(Event event) {
+    		cntI++;
+    		imagen = new Image(getClass().getResourceAsStream(pathImagenes[cntI%5]));
+    		imga.setImage(imagen);
+		}
+    }
+    
+    public Pane getPanel() {
+		return p0;
+	}
 }

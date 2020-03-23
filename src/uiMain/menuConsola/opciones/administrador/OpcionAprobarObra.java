6ab4365373e3b923f129a46a8271fa395cf1c2c7
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:09:07 -0500

    Update OpcionAprobarObra.java

--- src/uiMain/menuConsola/opciones/administrador/OpcionAprobarObra.java
+++ src/uiMain/menuConsola/opciones/administrador/OpcionAprobarObra.java
@@ -1,29 +1,24 @@
-package uiMain.menuConsola.opciones.administrador;
-
-import gestorAplicacion.Obras.Obra;
-import gestorAplicacion.Usuario.Administrador;
-import uiMain.menuConsola.MenuDeConsola;
-import uiMain.menuConsola.OpcionDeMenu;
-
-public class OpcionAprobarObra extends OpcionDeMenu {
-	
-	@Override
-	public String toString() {
-		return "Aprobar Obra";
-	}
-	
-	@Override
-	public MenuDeConsola ejecutar() {
-		
-		Obra obra = obraSelec(MenuDeConsola.getMenuActual().getAux());
-		Administrador.aprobarObra(obra);
-		System.out.println("Obra aprobada");
-		return null;
-		
-	}
-	@Override
-	public void asignar(MenuDeConsola atras,MenuDeConsola actual) {
-		actual.setAtras(atras);
-		atras.setSiguiente(actual);
-	}
-}
+//package uiMain.menuConsola.opciones.administrador;
+//
+//import gestorAplicacion.Obras.Obra;
+//import gestorAplicacion.Usuario.Administrador;
+//import gui.paneles.PaneInteraccion;
+//import uiMain.menuConsola.MenuDeConsola;
+//import uiMain.menuConsola.OpcionDeMenu;
+//
+//public class OpcionAprobarObra extends OpcionDeMenu {
+//	
+//	@Override
+//	public String toString() {
+//		return "Aprobar Obra";
+//	}
+//	
+//	@Override
+//	public void ejecutar() {
+//		
+//		Obra obra = obraSelec(PaneInteraccion.getPaneActual().getAux());
+//		Administrador.aprobarObra(obra);
+//		System.out.println("Obra aprobada");
+//		
+//	}
+//}

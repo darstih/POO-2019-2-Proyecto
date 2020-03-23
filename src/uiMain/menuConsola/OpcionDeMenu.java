1977fead7b4a1d64bb493343b6af1bc5fb137a6e
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:03:14 -0500

    Update OpcionDeMenu.java

--- src/uiMain/menuConsola/OpcionDeMenu.java
+++ src/uiMain/menuConsola/OpcionDeMenu.java
@@ -3,77 +3,58 @@ package uiMain.menuConsola;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Scanner;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 
+import Excepciones.CantBeNull;
+import Excepciones.NoCoincideTamaño;
 import gestorAplicacion.Obras.Etiqueta;
 import gestorAplicacion.Obras.Obra;
 import gestorAplicacion.Obras.Tecnica;
+import gui.paneles.FieldPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.FlowPane;
 
 public abstract class OpcionDeMenu{ // La clase abstracta obligatoria
 	
-	protected Scanner in = new Scanner(System.in);
+	private FieldPanel auxiliar;
 	
 	
-	abstract public MenuDeConsola ejecutar() throws JsonGenerationException, JsonMappingException, IOException;
+	
+	abstract public void ejecutar() throws JsonGenerationException, JsonMappingException, IOException;
 	
 	
-	protected Obra obraSelec(ArrayList<Obra> ao) {
-		System.out.println("Interfaz de vista de obras...");
-		System.out.println("Seleccione una obra: ");
-		if(ao == null) {
-			ao = Obra.getObras();
+	
+	public MenuItem toMenu() {
+		MenuItem a = new MenuItem(this.toString());
+		return a;
+	}
+	
+	
+	protected FlowPane mostrarEtiquetas(Obra o) {
+		FlowPane panel = new FlowPane();
+		for(Etiqueta i:o.getEtiquetas()) {
+			panel.getChildren().add(i.graficar());
 		}
-		
-		if(ao.isEmpty()) {
-			System.out.println("No hay obras");
-			return null;
-		}else {
-		for(int i = 0;i<ao.size();i++) {
-			System.out.println(i+". "+ao.get(i).getTitulo()+" del autor "+ao.get(i).getAutor());
-		}
-		int opcion=in.nextInt();
-		return(ao.get(opcion));}
+		return panel;
 	}
 	
-	protected Etiqueta mostrarEtiquetas(Obra o) {
-		for(int i =0;i<o.getEtiquetas().size();i++) {
-			System.out.println(i+". "+o.getEtiquetas().get(i).getLabel());
+	
+	protected FieldPanel formularioObra() throws NoCoincideTamaño, CantBeNull {
+		String[] criterios = new String[] {"Titulo","Descripcion","Altura","Ancho","Tecnica","Autor"};
+		CrearObraHandlerClass listener = new CrearObraHandlerClass();
+		return  new FieldPanel(this,"criterios",criterios,"valores",null,null,listener);
+	}
+	
+	
+	class CrearObraHandlerClass implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent arg0) {
+			new Obra(auxiliar.getValue("Titulo"), auxiliar.getValue("Descripcion"), Double.parseDouble(auxiliar.getValue("Altura")), Double.parseDouble(auxiliar.getValue("Ancho")), Calendar.getInstance(), new ArrayList<Etiqueta>(),new Tecnica( auxiliar.getValue("tecnica")), auxiliar.getValue("autor"));
 		}
-		Etiqueta e=o.getEtiquetas().get(in.nextInt());
-		return e;
 	}
-	protected 	Obra formObra() {
-		System.out.println("Agregando Obra, por favor ingresa los siguientes atributos:");
-		
-		System.out.println("T\u00EDtulo:");
-		String titulo = in.nextLine();
-		titulo += in.nextLine();
-		
-		System.out.println("Descripci\u00F3n:");
-		String descripcion = in.nextLine();
-		
-		System.out.println("Altura:");
-		double altura = in.nextDouble();
-		
-		System.out.println("Ancho:");
-		double ancho = in.nextDouble();
-		
-		System.out.println("T\u00E9cnica:");
-		String tec = in.nextLine();
-		tec += in.nextLine();
-		Tecnica tecnica = new Tecnica(tec);
-		
-		System.out.println("Autor:");
-		String autor = in.nextLine();
-		
-		Obra nuevaObra = new Obra(titulo, descripcion, altura, ancho, Calendar.getInstance(), new ArrayList<Etiqueta>(), tecnica, autor);
-		return nuevaObra;
-	}
-	public void asignar(MenuDeConsola atras,MenuDeConsola actual) {
-		actual.setAtras(atras);
-		atras.setSiguiente(actual);
-	}
+	
 }

30794b7926e038d6bc17fed3150353495e53739d
David Aristizabal <davidaristizabalgiraldo@gmail.com>
2020-03-22 22:23:07 -0500

    Cambio a interfaz grafica

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -10,22 +10,37 @@ import baseDatos.DBObra;
 import gestorAplicacion.Obras.Etiqueta;
 import gestorAplicacion.Obras.Obra;
 import gestorAplicacion.Obras.Tecnica;
+import gui.paneles.IndexPanel;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import uiMain.menuConsola.MenuDeConsola;
 
-public class Main {
-	public static void main (String [ ] args) throws JsonParseException, JsonMappingException, IOException {
+public class Main  extends Application{
+	Scene main1,main2;
 	
-		DBObra.inicializar();
-		System.out.println(Obra.getCantObras());
+	public static void main (String [ ] args) throws JsonParseException, JsonMappingException, IOException {
+		DBObra.inicializar();			
+		launch(args);
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception,JsonParseException, JsonMappingException, IOException {
+		IndexPanel a1 = new IndexPanel();
+		main1 = new Scene(a1.getPanel(),600,600);
+		primaryStage.setScene(main1);
+		primaryStage.show();
+		
+		//System.out.println(Obra.getCantObras());
 		// Para usar acentos y caracteres especiales en Strings:
 		// https://www.freeformatter.com/java-dotnet-escape.html
 		// Recordar que el '\' debe estar solo, ej. '\\' no sirve
 		
-		MenuDeConsola menu= MenuDeConsola.inicializar();
+		/*MenuDeConsola menu= MenuDeConsola.inicializar();
 		while (true) {
 			menu =  menu.lanzarMenu();
-		}
-		
+		}*/
 		
 	}
 }

ee09bce79a55cb496eea490a48f256a71dc009fb
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:07:33 -0500

    Update OpcionVolverInicio.java

--- src/uiMain/menuConsola/opciones/OpcionVolverInicio.java
+++ src/uiMain/menuConsola/opciones/OpcionVolverInicio.java
@@ -1,18 +1,28 @@
 package uiMain.menuConsola.opciones;
 
-import uiMain.menuConsola.MenuDeConsola;
+import gui.escena.EscenaInicial;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import uiMain.menuConsola.OpcionDeMenu;
 
 public class OpcionVolverInicio extends OpcionDeMenu {
 	
 	@Override
 	public String toString() {
-		return "Volver al inicio";
+		return "Salir";
 	}
 	
 	@Override
-	public MenuDeConsola ejecutar() {
-		return MenuDeConsola.inicializar();
+	public void ejecutar() {
+		EscenaInicial.init();
+	}
+	
+	public Pane toGrafic() {
+		Pane a = new Pane();
+		Button b = new Button(this.toString());
+		a.getChildren().add(b);
+		return a;
+		
 	}
 	
 }

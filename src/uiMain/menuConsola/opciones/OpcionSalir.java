8bb306b0dc0a0904368b160eb595e7ab4118c41e
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:06:00 -0500

    Update OpcionSalir.java

--- src/uiMain/menuConsola/opciones/OpcionSalir.java
+++ src/uiMain/menuConsola/opciones/OpcionSalir.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
-
 import baseDatos.DBObra;
-import uiMain.menuConsola.MenuDeConsola;
 import uiMain.menuConsola.OpcionDeMenu;
 
 public class OpcionSalir extends OpcionDeMenu {
@@ -15,16 +13,12 @@ public class OpcionSalir extends OpcionDeMenu {
 	public String toString() {
 		return "Salir";
 	}
-
+	
 	@Override
-	public MenuDeConsola ejecutar() throws JsonGenerationException, JsonMappingException, IOException {
-		// Para usar acentos y caracteres especiales en Strings:
-		// https://www.freeformatter.com/java-dotnet-escape.html
-		// Recordar que el '\' debe estar solo, ej. '\\' no sirve
+	public void ejecutar() throws JsonGenerationException, JsonMappingException, IOException {
 		System.out.println("\u00A1Chao! \u00A1Gracias por usar M\u0171vez\u00E1rt!");
 		DBObra.guardar();
 		System.exit(0);
-		return null;
 	}
 
 }

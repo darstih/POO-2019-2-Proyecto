c026f526a1e1899de37baa9f50a375533ca05055
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:08:14 -0500

    Update OpcionAgregarObra.java

--- src/uiMain/menuConsola/opciones/administrador/OpcionAgregarObra.java
+++ src/uiMain/menuConsola/opciones/administrador/OpcionAgregarObra.java
@@ -1,10 +1,28 @@
 package uiMain.menuConsola.opciones.administrador;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import Excepciones.CantBeNull;
+import Excepciones.NoCoincideTamaño;
+import gestorAplicacion.Obras.Etiqueta;
 import gestorAplicacion.Obras.Obra;
+import gestorAplicacion.Obras.Tecnica;
 import gestorAplicacion.Usuario.Administrador;
-import uiMain.menuConsola.MenuDeConsola;
+import gestorAplicacion.Usuario.Usuario;
+import gui.paneles.PaneInteraccion;
+import gui.paneles.FieldPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Pane;
+import javafx.stage.StageStyle;
 import uiMain.menuConsola.OpcionDeMenu;
 
+
+
+	
 public class OpcionAgregarObra extends OpcionDeMenu {
 	
 	
@@ -14,17 +32,32 @@ public class OpcionAgregarObra extends OpcionDeMenu {
 	}
 	
 	@Override
-	public MenuDeConsola ejecutar() {
-		
-		Obra nuevaObra = formObra();
-		Administrador.agregarObra(nuevaObra);
-		System.out.println("\nObra agregada:\n" + nuevaObra + "\n");
-		return null;
-		
+	public void ejecutar() throws NoCoincideTamaño, CantBeNull {
+		String[] criterios = new String[] {"Titulo","Descripcion","Altura","Ancho","Tecnica","Autor"};
+		CrearObraHandler hand = new CrearObraHandler();
+		FieldPanel a = new FieldPanel(this,"criterios",criterios,"valores",null,null,hand);
+		PaneInteraccion.setPaneActual(a);
 	}
-	@Override
-	public void asignar(MenuDeConsola atras,MenuDeConsola actual) {
-		atras.setSiguiente(atras);
-		atras.setAtras(atras.getAtras());
+	
+	
+	
+	class CrearObraHandler implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent arg0) {//Si este handler se activa es porque se está mostrando por lo tanto es el actual
+			try {
+				Pane pane = PaneInteraccion.getPaneActual();
+				Administrador.agregarObra(new Obra(((FieldPanel) pane).getValue("Titulo"), ((FieldPanel) pane).getValue("Descripcion") ,Double.parseDouble(((FieldPanel) pane).getValue("Altura")), Double.parseDouble(((FieldPanel) pane).getValue("Ancho")),Calendar.getInstance(),new ArrayList<Etiqueta>(), new Tecnica(((FieldPanel) pane).getValue("Tecnica")), ((FieldPanel) pane).getValue("Autor")));
+				Alert dialogo = new Alert(AlertType.INFORMATION);
+				dialogo.setTitle("Obra agregada correctamente");
+				dialogo.setContentText("La obra se agrego correctamente");
+				dialogo.initStyle(StageStyle.UTILITY);
+				dialogo.showAndWait();
+				PaneInteraccion.setPaneActual(Usuario.listarObraGrafica(Obra.getObras(), 1));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+	
+	
 }

f9a2966b5a9c4ecef6c276a115f6c2587a347a50
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:11:35 -0500

    Update OpcionEnviarObra.java

--- src/uiMain/menuConsola/opciones/invitado/OpcionEnviarObra.java
+++ src/uiMain/menuConsola/opciones/invitado/OpcionEnviarObra.java
@@ -1,14 +1,24 @@
 package uiMain.menuConsola.opciones.invitado;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import Excepciones.CantBeNull;
+import Excepciones.NoCoincideTamaño;
 import gestorAplicacion.Obras.Etiqueta;
 import gestorAplicacion.Obras.Obra;
 import gestorAplicacion.Obras.Tecnica;
 import gestorAplicacion.Usuario.Invitado;
-import uiMain.menuConsola.MenuDeConsola;
+import gestorAplicacion.Usuario.Usuario;
+import gui.paneles.FieldPanel;
+import gui.paneles.PaneInteraccion;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.Pane;
+import javafx.stage.StageStyle;
 import uiMain.menuConsola.OpcionDeMenu;
+
 public class OpcionEnviarObra extends OpcionDeMenu {
 
 	@Override
@@ -17,22 +27,31 @@ public class OpcionEnviarObra extends OpcionDeMenu {
 	}
 	
 	@Override
-	public MenuDeConsola ejecutar() {
-		
-		Obra nuevaObra = formObra();
-		Invitado.postularObra(nuevaObra);
-		System.out.println("\nObra:\n" + nuevaObra);
-		System.out.println("\nEn proceso...");
-		System.out.println("Cuando la apruebe un administrador será exitosamente agregada.");
-		MenuDeConsola menuInvCopy = new MenuDeConsola("");
-		return menuInvCopy;
-		
-	}
-	@Override
-	public void asignar(MenuDeConsola atras,MenuDeConsola siguiente) {
-		atras.setAtras(atras);
-		atras.setSiguiente(atras.getAtras());
+	public void ejecutar() throws NoCoincideTamaño, CantBeNull {
+		String[] criterios = new String[] {"Titulo","Descripcion","Altura","Ancho","Tecnica","Autor"};
+		CrearObraHandler hand = new CrearObraHandler();
+		FieldPanel a = new FieldPanel(this,"criterios",criterios,"valores",null,null,hand);
+		PaneInteraccion.setPaneActual(a);
 	}
 	
-
+	
+	
+	class CrearObraHandler implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent arg0) {//Si este handler se activa es porque se está mostrando por lo tanto es el actual
+			try {
+				FieldPanel pane = (FieldPanel) PaneInteraccion.getPaneActual();
+				Invitado.postularObra(new Obra(pane.getValue("Titulo"), pane.getValue("Descripcion") ,Double.parseDouble(pane.getValue("Altura")), Double.parseDouble( pane.getValue("Ancho")),Calendar.getInstance(),new ArrayList<Etiqueta>(), new Tecnica( pane.getValue("Tecnica")), pane.getValue("Autor")));
+				Alert dialogo = new Alert(AlertType.INFORMATION);
+				dialogo.setTitle("Obra agregada correctamente");
+				dialogo.setContentText("Cuando la apruebe un administrador será exitosamente agregada.");
+				dialogo.initStyle(StageStyle.UTILITY);
+				dialogo.showAndWait();
+				PaneInteraccion.setPaneActual(Usuario.listarObraGrafica(Obra.getObras(), 1));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				System.out.println("Error por formato del número");
+			}
+		}
+	}
 }

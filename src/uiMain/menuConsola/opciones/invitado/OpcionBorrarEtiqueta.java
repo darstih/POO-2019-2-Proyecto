97d886c2b89ac2e72888711dab35a603e46a86ea
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:11:00 -0500

    Update OpcionBorrarEtiqueta.java

--- src/uiMain/menuConsola/opciones/invitado/OpcionBorrarEtiqueta.java
+++ src/uiMain/menuConsola/opciones/invitado/OpcionBorrarEtiqueta.java
@@ -1,37 +1,32 @@
-package uiMain.menuConsola.opciones.invitado;
-
-import java.util.ArrayList;
-
-import gestorAplicacion.Obras.Etiqueta;
-import gestorAplicacion.Obras.Obra;
-import gestorAplicacion.Usuario.Invitado;
-import uiMain.menuConsola.MenuDeConsola;
-import uiMain.menuConsola.OpcionDeMenu;
-
-public class OpcionBorrarEtiqueta extends OpcionDeMenu {
-		
-	@Override
-	public String toString() {
-		return "Borrar Etiqueta";
-	}
-	
-	@Override
-	public MenuDeConsola ejecutar() {
-		
-		Obra obra = obraSelec(MenuDeConsola.getMenuActual().getAux());
-		System.out.println("Seleccione una etiqueta: ");
-		Etiqueta e=mostrarEtiquetas(obra);
-		Invitado.borrarEtiqueta(obra, e);
-		System.out.println("Etiqueta borrada");
-		return null;
-		
-	}
-	
-	@Override
-	public void asignar(MenuDeConsola atras,MenuDeConsola actual) {
-		atras.setSiguiente(atras);
-		atras.setAtras(atras.getAtras());
-	}
-
-
-}
+//package uiMain.menuConsola.opciones.invitado;
+//
+//import java.util.ArrayList;
+//
+//import gestorAplicacion.Obras.Etiqueta;
+//import gestorAplicacion.Obras.Obra;
+//import gestorAplicacion.Usuario.Invitado;
+//import gui.paneles.FieldPanel;
+//import javafx.scene.layout.Pane;
+//import uiMain.menuConsola.MenuDeConsola;
+//import uiMain.menuConsola.OpcionDeMenu;
+//
+//public class OpcionBorrarEtiqueta extends OpcionDeMenu {
+//		
+//	@Override
+//	public String toString() {
+//		return "Borrar Etiqueta";
+//	}
+//	
+//	@Override
+//	public Pane ejecutar() {
+//		Obra obra = obraSelec(FieldPanel.getAuxObra());
+//		System.out.println("Seleccione una etiqueta: ");
+//		Etiqueta e=mostrarEtiquetas(obra);
+//		Invitado.borrarEtiqueta(obra, e);
+//		System.out.println("Etiqueta borrada");
+//		return null;
+//	}
+//	
+//
+//
+//}

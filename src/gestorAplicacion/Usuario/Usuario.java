0b2d10092527ebe93709bf404b9f299ac08b6d2a
darstih <48297481+darstih@users.noreply.github.com>
2020-03-23 03:31:53 -0500

    Update Usuario.java

--- src/gestorAplicacion/Usuario/Usuario.java
+++ src/gestorAplicacion/Usuario/Usuario.java
@@ -5,92 +5,106 @@ import java.util.Collections;
 
 import gestorAplicacion.Excepciones.ExcepcionFueraRango;
 import gestorAplicacion.Obras.Obra;
+import javafx.scene.layout.FlowPane;
 
 
 //Autor clase y estructura Darwin Herrera
 public abstract class Usuario{
-	
-    //Atributos
+
+	//Atributos
 
 
-    // listado = 1 | ordena las obras por su fecha de creacion, de mas antiguo a mas nuevo
-    // listado = 2 | ordena por relevancia, siendo comentarios su criterio de relevancia mas comentarios a menos
-    // listado = 3 | ordena las obras por su fecha de ingreso al sistema, de mas antiguo a mas nuevo
-    private static ArrayList<Obra> listarObra(ArrayList<Obra> listaO, int listado){
-	if(listado ==1){
-	    Collections.sort(listaO,(o1,o2) -> {
-		    return o1.getFechaCreacion().compareTo(o2.getFechaCreacion());
-		});
-	    return listaO;
-	}else if(listado ==2){
-	    Collections.sort(listaO,(o1,o2) -> {
-		    return Integer.compare(o1.devolverCantComentarios()*-1,o2.devolverCantComentarios()*-1);
-		});
-	    return listaO;
-	}else if(listado == 3){
-	    Collections.sort(listaO,(o1,o2) -> {
-		    return o1.getFechaIngreso().compareTo(o2.getFechaIngreso());
-		});
-	    return listaO;
+	// listado = 1 | ordena las obras por su fecha de creacion, de mas antiguo a mas nuevo
+	// listado = 2 | ordena por relevancia, siendo comentarios su criterio de relevancia mas comentarios a menos
+	// listado = 3 | ordena las obras por su fecha de ingreso al sistema, de mas antiguo a mas nuevo
+	private static ArrayList<Obra> listarObra(ArrayList<Obra> listaO, int listado){
+		if(listado ==1){
+			Collections.sort(listaO,(o1,o2) -> {
+				return o1.getFechaCreacion().compareTo(o2.getFechaCreacion());
+			});
+			return listaO;
+		}else if(listado ==2){
+			Collections.sort(listaO,(o1,o2) -> {
+				return Integer.compare(o1.devolverCantComentarios()*-1,o2.devolverCantComentarios()*-1);
+			});
+			return listaO;
+		}else if(listado == 3){
+			Collections.sort(listaO,(o1,o2) -> {
+				return o1.getFechaIngreso().compareTo(o2.getFechaIngreso());
+			});
+			return listaO;
+		}
+		return listaO;
 	}
-	return listaO;
-    }
 
-    //Autor Darwin Herrera
-    private static ArrayList<Obra> buscarObraPorTecnica(String var,int listado){//eficiencia O(n)
-	ArrayList<Obra> a = Obra.getObras();
-	ArrayList<Obra> res=new ArrayList<Obra>();
-	for(int i=0;i<a.size();i++) {
-	    if(a.get(i).getTecnica().getNombre()==var) {
-		res.add(a.get(i));
-	    }
+
+
+
+
+	public static FlowPane listarObraGrafica(ArrayList<Obra> obras,int listado) {
+		FlowPane pane = new FlowPane();
+		pane.setVgap(10);
+		pane.setHgap(10);
+		obras= listarObra(obras,listado);
+		for(Obra i:obras) {
+			pane.getChildren().add(i.graficar());
+		}
+		return pane;
 	}
-	return listarObra(res,listado);
-    }
-    private static ArrayList<Obra> buscarObraPorTitulo(String var,int listado){//eficiencia O(n)
-	ArrayList<Obra> a = Obra.getObras();
-	ArrayList<Obra> res=new ArrayList<Obra>();
-	for(int i=0;i<a.size();i++) {
-	    if(a.get(i).getTitulo()==var) {
-		res.add(a.get(i));
-	    }
+	//Autor Darwin Herrera
+	private static FlowPane buscarObraPorTecnica(String var,int listado){//eficiencia O(n)
+		ArrayList<Obra> a = Obra.getObras();
+		ArrayList<Obra> res=new ArrayList<Obra>();
+		for(int i=0;i<a.size();i++) {
+			if(a.get(i).getTecnica().getNombre()==var) {
+				res.add(a.get(i));
+			}
+		}
+		return listarObraGrafica(res,listado);
 	}
-	return listarObra(res,listado);
-    }
-	
-	
-	
-	
-	
-	
-	
-	
-    // listado = 1 | ordena las obras por su fecha de creacion, de mas antiguo a mas nuevo
-    // listado = 2 | ordena por relevancia, siendo comentarios su criterio de relevancia mas comentarios a menos
-    // listado = 3 | ordena las obras por su fecha de ingreso al sistema, de mas antiguo a mas nuevo
-	
-    //Autor Darwin Herrera
-    public static ArrayList<Obra> buscarObra(String var,int i,int listado) throws ExcepcionFueraRango{
-	try {
-	    if(i==0) {
-		return buscarObraPorTecnica(var,listado);
-	    }else if(i==1) {
-		return buscarObraPorTitulo(var,listado);
-	    }else {
-		throw new ExcepcionFueraRango();
-	    }
-	}catch(ExcepcionFueraRango e) {
-	    return null;
+	private static FlowPane buscarObraPorTitulo(String var,int listado){//eficiencia O(n)
+		ArrayList<Obra> a = Obra.getObras();
+		ArrayList<Obra> res=new ArrayList<Obra>();
+		for(int i=0;i<a.size();i++) {
+			if(a.get(i).getTitulo()==var) {
+				res.add(a.get(i));
+			}
+		}
+		return listarObraGrafica(res,listado);
 	}
-    }
-    //Metodos
-	
-    //Getters y setters
-	
-    //Constructores
-    public Usuario() {
-		
-    }
-	
+
+
+
+
+
+
+
+
+	// listado = 1 | ordena las obras por su fecha de creacion, de mas antiguo a mas nuevo
+	// listado = 2 | ordena por relevancia, siendo comentarios su criterio de relevancia mas comentarios a menos
+	// listado = 3 | ordena las obras por su fecha de ingreso al sistema, de mas antiguo a mas nuevo
+
+	//Autor Darwin Herrera
+	public static FlowPane buscarObra(String var,int i,int listado) throws ExcepcionFueraRango{
+		try {
+			if(i==0) {
+				return buscarObraPorTecnica(var,listado);
+			}else if(i==1) {
+				return buscarObraPorTitulo(var,listado);
+			}else {
+				throw new ExcepcionFueraRango();
+			}
+		}catch(ExcepcionFueraRango e) {
+			return null;
+		}
+	}
+	//Metodos
+
+	//Getters y setters
+
+	//Constructores
+	public Usuario() {
+
+	}
+
 }
-

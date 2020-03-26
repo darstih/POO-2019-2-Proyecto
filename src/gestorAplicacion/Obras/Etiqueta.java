6e0504e5f6d5cee915e19eeeed3e1a218a683ce6
David Aristizabal <davidaristizabalgiraldo@gmail.com>
2020-03-26 01:33:20 -0500

    lista de etiquetas con sus funciones para manipularla

--- src/gestorAplicacion/Obras/Etiqueta.java
+++ src/gestorAplicacion/Obras/Etiqueta.java
@@ -1,17 +1,16 @@
 package gestorAplicacion.Obras;
 
-import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import java.util.ArrayList;
+
 //Autor Darwin Herrera
 public class Etiqueta {
 	//Atributos
 	private String tipo;
 	private String descripcion;
 	private String label;
-	int cantObras=0;
-
+	private int cantObras=0;
+	
+	private static ArrayList<Etiqueta> listaEtiquetas = new ArrayList<Etiqueta>();
 	public Etiqueta() {}
 	//Metodos
 	
@@ -40,11 +39,21 @@ public class Etiqueta {
 	public void setTipo(String tipo){
 		this.tipo=tipo;
 	}
-
+	public void setCantObras(int cantObras) {
+		this.cantObras = cantObras;
+	}
 	public int getCantObras(){
 		return cantObras;
 	}
-	
+	public static void setEtiquetas(ArrayList<Etiqueta> et) {
+		listaEtiquetas = et;
+	}
+	public static ArrayList<Etiqueta> getEtiquetas(){
+		return listaEtiquetas;
+	}
+	public void aumentarCantObras() {
+		this.cantObras++;
+	}
 	//Constructores
 	public Etiqueta(String label, String tipo, String descripcion) {
 		this.label = label;
@@ -54,5 +63,28 @@ public class Etiqueta {
 	public Etiqueta(String labol, String tipo) {
 		new Etiqueta(labol, tipo,"Sin descripción");
 	}
+	public static void setEtiqueta(Etiqueta etiqueta) {
+		listaEtiquetas.add(etiqueta);
+	}
+	public static void aumentarContador(Etiqueta etiqueta) {
+		for(int i = 0; i<listaEtiquetas.size();i++) {
+			if(listaEtiquetas.get(i).getLabel().equalsIgnoreCase(etiqueta.getLabel())) {
+				int aux = listaEtiquetas.get(i).getCantObras()+1;
+				listaEtiquetas.get(i).setCantObras(aux);;
+				break;
+			}
+		}
+	}
 	
+	//Aca verificamos si una etiqueta ya existe
+	public static boolean verificarEtiqueta(Etiqueta etiqueta){
+		boolean retornar = false;
+		for (int i=0;i<listaEtiquetas.size();i++){
+			if(etiqueta.getLabel().equalsIgnoreCase(listaEtiquetas.get(i).getLabel())){
+				retornar=true;			
+				break;
+			}
+		}
+		return retornar;
+	}
 }
